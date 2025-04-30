4ffe25facde53f93751f5036a51d9866e854055f
Daniel Vargas <daniel.rvargas10@gmail.com>
2025-04-29 23:55:50 -0300

    feat: add entities and relationships - Category, Order, OrderItem, OrderItemPK, OrderStatus, Payment, Product and User

---
+++ src/main/java/br/com/devsuperior/dscommerce/entitites/Order.java
@@ -0,0 +1,68 @@
+package br.com.devsuperior.dscommerce.entitites;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Order() {}
+
+    public Order(Long id, Instant moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).toList();
+    }
+}
---
+++ src/main/java/br/com/devsuperior/dscommerce/entitites/OrderStatus.java
@@ -0,0 +1,9 @@
+package br.com.devsuperior.dscommerce.entitites;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
