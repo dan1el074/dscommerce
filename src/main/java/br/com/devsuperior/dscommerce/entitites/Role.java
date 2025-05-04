2815c3c5d357f2ed5ebec0406012cc59e79dcb04
Daniel Vargas <daniel.rvargas10@gmail.com>
2025-05-04 17:19:25 -0300

    feat: add Role entity and update seed

---
+++ src/main/java/br/com/devsuperior/dscommerce/entitites/Role.java
@@ -0,0 +1,54 @@
+package br.com.devsuperior.dscommerce.entitites;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() {}
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+        return Objects.equals(getAuthority(), role.getAuthority());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getAuthority());
+    }
+}
